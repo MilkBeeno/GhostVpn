c5975296704517243534e543f82f24410f4452c2
MilkBeeno <milkbeeno@gmail.com>
2023-03-03 15:32:21 +0800

    我的页面实现

---
+++ app/src/main/java/com/simple/ghostvpn/activity/MyActivity.kt
@@ -0,0 +1,48 @@
+package com.simple.ghostvpn.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.milk.simple.ktx.immersiveStatusBar
+import com.milk.simple.ktx.statusBarPadding
+import com.simple.ghostvpn.R
+
+class MyActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var ivBack: AppCompatImageView
+    private lateinit var llPrivacy: LinearLayoutCompat
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+        immersiveStatusBar()
+
+        ivBack = findViewById(R.id.ivBack)
+        ivBack.statusBarPadding()
+        ivBack.setOnClickListener(this)
+
+        llPrivacy = findViewById(R.id.llPrivacy)
+        llPrivacy.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            ivBack -> {
+                finish()
+            }
+            llPrivacy -> {
+                WebViewActivity.start(this)
+            }
+        }
+    }
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, MyActivity::class.java))
+        }
+    }
+}
